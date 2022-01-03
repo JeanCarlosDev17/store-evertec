8249a05f03cadd9481d8144ed25e5e7753e113b3
Jean Carlos <jeancarlosrecio@hotmail.com>
2022-01-02 19:51:46 -0500

    feature implementado admin lte

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,5 +1,5 @@
 @if((auth()->user()->role_id ==1))
-<x-app-layout>
+{{--<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -10,14 +10,33 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in Admin View!
+
+
+
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-app-layout>--}}
+
+@extends('adminlte::page')
+
+@section('title', 'Administrador')
+
+@section('content_header')
+    <h1>Administrador</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido al panel de administración </p>
+    crud
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
 @else
     <h2>ACCESO NO AUTORIZADO... </h2>
 {{--    TODO despues implementar policies--}}
-    <a href="/dashboard" class="btn btn-blue">Regresar</a>
+    <a href="/" class="btn btn-blue">Regresar</a>
 @endif
