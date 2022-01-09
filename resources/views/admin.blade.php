ccd067412aac33729216001ef18605077399a57b
jean carlos <jeancarlosrecio@hotmail.com>
2022-01-08 22:08:41 -0500

    Feat: añadido Confirmacion para editar formulario

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,5 +1,7 @@
 @if (isset(auth()->user()->role) and auth()->user()->role == 'admin')
 
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.3.4/dist/sweetalert2.min.css">
 
     @extends('adminlte::page')
 
@@ -17,12 +19,59 @@
 
             </x-indexUser>
         </div>
+        <script>
+            let formsEdit=document.querySelectorAll('form.edit');
+            let formsDelete=document.querySelectorAll('form.delete');
+            let formsState=document.querySelectorAll('form.state');
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                    confirmButton: 'btn btn-success',
+                    cancelButton: 'btn btn-danger mr-3'
+                },
+                buttonsStyling: false
+            })
 
+            formsEdit.forEach(form => {
+               form.addEventListener('submit',(event)=>{
+
+
+                   swalWithBootstrapButtons.fire({
+                       title: 'Esta seguro que desea Editar este Usuario',
+                       text: "",
+                       icon: 'warning',
+                       showCancelButton: true,
+                       confirmButtonText: 'Si, Confirmar',
+                       cancelButtonText: 'No, Cancelar!',
+                       reverseButtons: true
+                   }).then((result) => {
+                       if (result.isConfirmed) {
+                           console.log("confirmado");
+                           form.submit();
+                           respuesta= true;
+                       } else if (
+                           /* Read more about handling dismissals below */
+                           result.dismiss === Swal.DismissReason.cancel
+                       ) {
+                           /*swalWithBootstrapButtons.fire(
+                               'Cancelado',
+                               '',
+                               'error'
+                           )*/
+                       }
+                   })
+
+                  event.preventDefault();
+               });
+            });
+        </script>
     @stop
 
     @section('css')
         <link rel="stylesheet" href="/css/admin_custom.css">
     @stop
+
+
+
 @else
     <h2>ACCESO NO AUTORIZADO... </h2>
     {{-- TODO despues implementar policies --}}
--- resources/views/components/indexUser.blade.php
+++ resources/views/components/indexUser.blade.php
@@ -24,12 +24,21 @@
             <td>{{$user->email}}</td>
             <td>{{$user->role=='admin' ? "Administrador":"Usuario"}}</td>
             <td>{{$user->user_state==1 ? "Activo":"Inactivo"}}</td>
-            <td><a type="button" href="/admin/users/{{$user->id}}/edit" class="btn btn-info "><i class="fas fa-edit" style="max-width:min-content"></i>  </a></td>
-            <td><form action="{{route('users.destroy',$user->id)}}" method="post">
+            <td>
+                <form action="{{route('users.edit',$user->id)}}" class="edit">
+                    <button type="submit" class="btn btn-info "><i class="fas fa-edit" style="max-width:min-content"></i>  </button>
+                </form>
+
+            </td>
+
+
+            <td><form action="{{route('users.destroy',$user->id)}}" class="delete" method="post">
                     @csrf
                     @method('DELETE')
 
-                <button type="submit"  class="btn btn-danger "><i class="fas fa-user-times" style="max-width:min-content"></i>  </button></td>
+                    <button type="submit"  class="btn btn-danger "><i class="fas fa-user-times" style="max-width:min-content"></i></button>
+                </form>
+            </td>
                 </form>
 
             <td class="d-flex justify-content-center "><a type="button"  href="" class="btn btn-warning text-center  " style="max-width:min-content "><i class="fas fa-toggle-on font-weight-bold  " style="color: black"> </i></a></td>
