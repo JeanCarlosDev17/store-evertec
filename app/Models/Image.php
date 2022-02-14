9d55e66c7062ba384b0518b905897a5ea5b57514
jean carlos <jeancarlosrecio@hotmail.com>
2022-02-13 20:14:32 -0500

    refactor(products): add softdelte to images

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
 
 class Image extends Model
 {
+    use SoftDeletes;
     use HasFactory;
     public function product()
     {
