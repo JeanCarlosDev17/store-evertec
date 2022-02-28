e0bf6c6c4c6e3ff07143f296db2e60c1e2969f84
jean carlos <jeancarlosrecio@hotmail.com>
2022-02-28 11:25:51 -0500

    Fix datatable responsive to products

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -8,7 +8,8 @@
 
 @section('content_header')
 
-    <x-admin.modalSuccess ></x-admin.modalSuccess>
+{{--    <x-admin.modalSuccess ></x-admin.modalSuccess>--}}
+    <x-admin.validationSuccess ></x-admin.validationSuccess>
     <h1>Administrador</h1>
 @stop
 
@@ -24,26 +25,26 @@
 
     </div>
 
-    <!-- Modal -->
-    <div class="modal fade" id="modalAlert" tabindex="-1" aria-labelledby="modalAlertLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modalAlertLabel">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    BORRADO CORRECTAMENTE
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
+{{--    <!-- Modal -->--}}
+{{--    <div class="modal fade" id="modalAlert" tabindex="-1" aria-labelledby="modalAlertLabel" aria-hidden="true">--}}
+{{--        <div class="modal-dialog modal-dialog-centered">--}}
+{{--            <div class="modal-content">--}}
+{{--                <div class="modal-header">--}}
+{{--                    <h5 class="modal-title" id="modalAlertLabel">Modal title</h5>--}}
+{{--                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">--}}
+{{--                        <span aria-hidden="true">&times;</span>--}}
+{{--                    </button>--}}
+{{--                </div>--}}
+{{--                <div class="modal-body">--}}
+{{--                    BORRADO CORRECTAMENTE--}}
+{{--                </div>--}}
+{{--                <div class="modal-footer">--}}
+{{--                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>--}}
+{{--                    <button type="button" class="btn btn-primary">Save changes</button>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 
 @stop
 
@@ -55,6 +56,15 @@
     <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">-->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap4.min.css">
+
+
+    <!--  Datatables  -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.20/datatables.min.css" />
+
+    <!--  extension responsive  -->
+    <link rel="stylesheet" type="text/css"
+          href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css">
+
 
     <style>
         .paginate_button{
@@ -69,25 +79,39 @@
 
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap4.min.js">
+    <script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap4.min.js"> </script>
     <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
     <script  src="https://cdn.datatables.net/responsive/2.2.9/js/responsive.bootstrap4.min.js"></script>
+
+
+
+    <!--   Datatables-->
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.20/datatables.min.js"></script>
+
+    <!-- extension responsive -->
+    <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+
+
+
     <script defer>
         $(document).ready(function() {
             $('#products').DataTable( {
-
+                responsive: true
             } );
         } );
 
     </script>
-    <script >
 
-        $(document).ready(function() {
-            $("#success-alert").fadeTo(4500, 500).slideUp(500, function(){
-                $("#success-alert").slideUp(500);
-            });
-        })
-    </script>
+
+
+{{--    <script >--}}
+
+{{--        $(document).ready(function() {--}}
+{{--            $("#success-alert").fadeTo(4500, 500).slideUp(500, function(){--}}
+{{--                $("#success-alert").slideUp(500);--}}
+{{--            });--}}
+{{--        })--}}
+{{--    </script>--}}
 
 
 @stop
--- resources/views/components/Admin/indexProducts.blade.php
+++ resources/views/components/Admin/indexProducts.blade.php
@@ -2,20 +2,103 @@
 <div class="card">
 
     <div class="card-body">
+
+{{--            <div class="row">--}}
+{{--                <div class="col-lg-12">--}}
+{{--                    <table id="example" class="table table-bordered  display " cellspacing="0" width="100%">--}}
+{{--                        @csrf--}}
+{{--                        <thead>--}}
+{{--                        <tr>--}}
+
+{{--                            <th scope="col" >id</th>--}}
+{{--                            <th scope="col" >Nombre</th>--}}
+{{--                            <th scope="col">Descripción</th>--}}
+{{--                            <th scope="col">Fabricante</th>--}}
+{{--                            <th scope="col">Cantidad</th>--}}
+{{--                            <th scope="col">Estado</th>--}}
+{{--                            <th scope="col">Editar</th>--}}
+{{--                            <th scope="col">Eliminar</th>--}}
+{{--                            <th scope="col">Des/Activar</th>--}}
+{{--                        </tr>--}}
+{{--                        </thead>--}}
+{{--                        <tbody>--}}
+{{--                        @forelse($products as $product)--}}
+{{--                            <tr>--}}
+{{--                                <td>{{$product->id}}</td>--}}
+{{--                                <td>{{$product->name}}</td>--}}
+{{--                                <td>{{$product->description}}</td>--}}
+{{--                                <td>{{$product->maker}}</td>--}}
+{{--                                <td>{{$product->quantity}}</td>--}}
+{{--                                <td>{{$product->state}}</td>--}}
+{{--                                <td>--}}
+{{--                                    <form action="{{route('products.edit',$product->id)}}" class="editProduct d-flex" method="get">--}}
+{{--                                        @csrf--}}
+{{--                                        @method('GET')--}}
+{{--                                        <button type="submit" class="btn btn-info text-center  mx-auto  ">--}}
+{{--                                            <i class="fas fa-edit " style="max-width:min-content"></i>--}}
+{{--                                        </button>--}}
+{{--                                    </form>--}}
+{{--                                </td>--}}
+{{--                                <td class=" justify-content-center ">--}}
+{{--                                    <form action="{{route('products.destroy',$product->id)}}" class="deleteProduct d-flex " method="post">--}}
+{{--                                        @csrf--}}
+{{--                                        @method('DELETE')--}}
+
+{{--                                        <button type="submit"  class="btn btn-danger text-center  mx-auto  ">--}}
+{{--                                            <i class="fas fa-minus-circle" style="max-width:min-content"></i>--}}
+{{--                                        </button>--}}
+{{--                                    </form>--}}
+{{--                                </td>--}}
+
+
+{{--                                <td class=" justify-content-center ">--}}
+{{--                                    <form action="{{route('products.state',$product->id)}}" class="  stateProduct d-flex" method="post">--}}
+{{--                                        @csrf--}}
+{{--                                        @method('PUT')--}}
+{{--                                        <button type="submit"  href="" class="btn btn-{{$product->state=='active' ? "warning":"success"}} text-center  mx-auto " style="max-width:min-content;  ">--}}
+{{--                                            <i class="fas fa-toggle-on font-weight-bold  " style="color: black"> </i>--}}
+{{--                                        </button>--}}
+{{--                                    </form>--}}
+{{--                                </td>--}}
+
+{{--                        @empty--}}
+{{--                            <h2>No hay Productos registrados</h2>--}}
+{{--                        @endforelse--}}
+
+
+{{--                        </tbody>--}}
+{{--                        <tfoot>--}}
+{{--                        <tr>--}}
+{{--                            <th scope="col">Nombre</th>--}}
+{{--                            <th scope="col">Descripción</th>--}}
+{{--                            <th scope="col">Fabricante</th>--}}
+{{--                            <th scope="col">Cantidad</th>--}}
+{{--                            <th scope="col">Estado</th>--}}
+{{--                            <th scope="col">Des/Activar</th>--}}
+{{--                            <th scope="col">Editar</th>--}}
+{{--                            <th scope="col">Eliminar</th>--}}
+
+{{--                        </tr>--}}
+{{--                        </tfoot>--}}
+{{--                    </table>--}}
+
+{{--                </div>--}}
+{{--            </div>--}}
+
         <div class="table-responsive-md">
-            <table class="table table-striped table-bordered dt-responsive" id="products">
+            <table class="table table-striped table-bordered dt-responsive display " id="products" cellspacing="0" width="100%" style="width: 100% !important;">
                 @csrf
                 <thead class="bg-primary text-white">
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Nombre</th>
-                    <th scope="col">Descipción</th>
+                    <th scope="col">Descripción</th>
                     <th scope="col">Fabricante</th>
                     <th scope="col">Cantidad</th>
                     <th scope="col">Estado</th>
+                    <th scope="col">In/Activar</th>
                     <th scope="col">Editar</th>
                     <th scope="col">Eliminar</th>
-                    <th scope="col">Activar/Desactivar</th>
 
                 </tr>
                 </thead>
@@ -28,37 +111,38 @@
                         <td>{{$product->maker}}</td>
                         <td>{{$product->quantity}}</td>
                         <td>{{$product->state}}</td>
-
-                        <td>
-                            <form action="{{route('products.edit',$product->id)}}" class="editProduct" method="get">
-                                @csrf
-                                @method('GET')
-                                <button type="submit" class="btn btn-info "><i class="fas fa-edit" style="max-width:min-content"></i>  </button>
-                            </form>
-
-                        </td>
-
-
-                        <td><form action="{{route('products.destroy',$product->id)}}" class="deleteProduct" method="post">
-                                @csrf
-                                @method('DELETE')
-
-                                <button type="submit"  class="btn btn-danger "><i class="fas fa-minus-circle" style="max-width:min-content"></i></button>
-                            </form>
-                        </td>
-
-
-                        <td class="d-flex justify-content-center ">
-
-                            <form action="{{route('products.state',$product->id)}}" class="stateProduct" method="post">
-
+                        <td class=" justify-content-center ">
+                            <form action="{{route('products.state',$product->id)}}" class="stateProduct d-flex" method="post">
                                 @csrf
                                 @method('PUT')
-                                <button type="submit"  href="" class="btn btn-{{$product->state=='active' ? "warning":"success"}} text-center  " style="max-width:min-content ">
+                                <button type="submit"  href="" class="btn btn-{{$product->state=='active' ? "warning":"success"}} text-center   mx-auto " style="max-width:min-content ">
                                     <i class="fas fa-toggle-on font-weight-bold  " style="color: black"> </i>
                                 </button>
                             </form>
                         </td>
+
+                        <td>
+                            <form action="{{route('products.edit',$product->id)}}" class="editProduct d-flex" method="get">
+                                @csrf
+                                @method('GET')
+                                <button type="submit" class="btn btn-info text-center  mx-auto">
+                                    <i class="fas fa-edit" style="max-width:min-content"></i>
+                                </button>
+                            </form>
+                        </td>
+
+                        <td>
+                            <form action="{{route('products.destroy',$product->id)}}" class="deleteProduct d-flex" method="post">
+                                @csrf
+                                @method('DELETE')
+
+                                <button type="submit"  class="btn btn-danger text-center  mx-auto ">
+                                    <i class="fas fa-minus-circle " style="max-width:min-content"></i>
+                                </button>
+                            </form>
+                        </td>
+
+
 
                     </tr>
                 @empty
